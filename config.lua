4c9293ddfc114d2ef43dbd48cb738e86f3188b24
Anaodasdasd <davidlajos303@gmail.com>
2025-08-18 12:05:23 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,9 @@
+return {
+    server = "127.0.0.1",
+    port = 8080,
+    admins = {"Peter", "Jozef", "Marek"},
+    settings = {
+        maxPlayers = 32,
+        motd = "Vitaj na servery!"
+    }
+}
