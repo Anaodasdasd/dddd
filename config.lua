1d96e1a1c8d40043d4a43a39685ea8c3475cf498
Anaodasdasd <davidlajos303@gmail.com>
2025-08-24 19:02:04 +0200

    Delete config.lua

--- config.lua
+++
@@ -1,9 +0,0 @@
-return {
-    server = "127.0.0.1",
-    port = 8080,
-    admins = {"Peter", "Jozef", "Marek"},
-    settings = {
-        maxPlayers = 32,
-        motd = "Vitaj na servery!"
-    }
-}
